82869cf7966afbaf9a9c40895f46027fd0dad2e5
Srusti Gourani <79088553+srusti610@users.noreply.github.com>
2023-10-03 08:15:34 +0530

    Create swap_using_inbuilt_func.cpp

---
+++ c++/swap_using_inbuilt_func.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Driver code
+int main()
+{
+    int a = 5, b = 10;
+ 
+    cout << "Before swapping a = " << a << " , b = " << b
+         << endl;
+ 
+    // Built-in swap function
+    swap(a, b);
+ 
+    cout << "After swapping a = " << a << " , b = " << b
+         << endl;
+    return 0;
+}
